c6356d664f43d5deb2ffce8fd344af41c780dab0
Shivam Shinde <shivamshinde92722@gmail.com>
2022-06-03 00:49:41 +0530

    Created methods related to model layer creation, model fitting, model compiling, evaluation of model on test data, and prediction on new data  using fitted mode

--- ModelCreation/ModelCreation.py
+++ ModelCreation/ModelCreation.py
@@ -2,6 +2,8 @@ from tensorflow import keras
 from data.data_loader import DataLoader
 from Logging.LoggerFile import Logger
 import os
+import pandas as pd
+import numpy as np
 
 class ModelCreation:
 
@@ -10,8 +12,6 @@ class ModelCreation:
         # getting data
         self.X_train, self.X_valid, self.X_test, self.y_train, self.y_valid, self.y_test = DataLoader().getValidationData()
 
-        
-        
 
     def CreateModel(self):
 
@@ -33,17 +33,17 @@ class ModelCreation:
             Logger().log(f, 'Layers created successfully!')
             Logger().log(f, 'Creating a model...')
             ## creating a model
-            self.model = keras.Model(inputs =[self.input_layer], outputs=[self.output_layer])
+            model = keras.Model(inputs =[self.input_layer], outputs=[self.output_layer])
             Logger.log(f, 'Model created successfully!')
             f.close()
-            return self.model
+            return model
 
         except Exception as e:
             Logger().log(f, f"Exception occured while creating layers or creating model. Exception: {str(e)}")
             f.close()
             raise e
 
-    def SaveModelArchitecture(self):
+    def SaveModelArchitecture(self, model):
 
         if not os.path.exists('LoggedData'):
             os.makedirs('LoggedData')
@@ -54,7 +54,7 @@ class ModelCreation:
             os.makedirs('ModelArchitecture')
 
         try:
-            model = self.CreateModel()
+            model = model
 
             keras.utils.save_model(model, os.path.join('ModelArchitecture', 'ModelStructure.png'))
             Logger().log(f, 'Model structure saved as a png file successfully!')
@@ -63,5 +63,111 @@ class ModelCreation:
         except Exception as e:
             Logger().log(f, f"Exception occured while saving the model structure as a png file. {str(e)}")
             raise e
+
+    def CompilingModel(self, model):
+
+        if not os.path.exists('LoggedData'):
+            os.makedirs('LoggedData')
+
+        f = open(os.path.join('LoggedData', 'ModelCreation.txt'), 'a+')
+
+        try:
+            
+            model = model
+            Logger().log(f, 'Compiling the model...')
+            model.compile(loss='sparse_categorical_crossentropy',
+                            optimizer='sgd',
+                            metrics=['accuracy'])
+            Logger.log(f, 'Model compiled successfully!')
+            f.close()
+            return model
+
+        except Exception as e:
+            Logger().log(f, f"Exception occured while compiling the model. Exception: {str(e)}")
+            f.close()
+            raise e
+            
+
+
+    def FittingModel(self, epochs=20, model=None):
+
+        if not os.path.exists('LoggedData'):
+            os.makedirs('LoggedData')
+
+        f = open(os.path.join('LoggedData', 'ModelCreation.txt'), 'a+')
+
+        try:
+
+            model = model
+            Logger().log(f, 'Fitting the model...')
+            history = model.fit(self.X_train, self.y_train,epochs=epochs, 
+                                        validation_data = (self.X_valid, self.y_valid) )
+
+            Logger().log(f, 'Model fitted successfully!')
+
+            if not os.path.exists('Plots'):
+                os.makedirs('Plots')
+            
+            history_df = pd.DataFrame(history.history)
+            figure = history_df.plot(figsize=(12,6)).get_figure()
+
+            figure.savefig(os.path.join('Plots', 'LossAndAccuracyVSepochsPlot'))
+            Logger.log(f, 'Saved the plot between the loss and accuracy of train and validation set with respect to number of epochs')
+            f.close()
+            return model
+
+        except Exception as e:
+            Logger().log(f, f"Exception occured while fitting the model to the training data. Exception: {str(e)}")
+            f.close()
+            raise e
+
     
+    def ModelEvaluationOnTestData(self, model):
+        
+        if not os.path.exists('LoggedData'):
+            os.makedirs('LoggedData')
+
+        f = open(os.path.join('LoggedData', 'ModelCreation.txt'), 'a+')
+
+        try:
+            self.model = model
+
+            model_loss, model_accuracy = self.model.evaluate(self.X_test, self.y_test)[0], self.model.evaluate(self.X_test, self.y_test)[1]
+
+            Logger().log(f, f"Model's loss on the test data: {np.round(model_loss,3)}")
+            Logger().log(f, f"Model's accuracy on the test data: {np.round(model_accuracy,3)}")
+
+            f.close()
+
+            print(f"Model's loss on the test data: {np.round(model_loss,3)}")
+            print(f"Model's accuracy on the test data: {np.round(model_accuracy,3)}")
+
+        except Exception as e:
+            Logger().log(f, f"Exception occured while evaluating the fitted model on the test data. Exception: {str(e)}")
+            f.close()
+            raise e
+
+    
+    def PredictionForNewData(self, input_array):
+
+        if not os.path.exists('LoggedData'):
+            os.makedirs('LoggedData')
+
+        f = open(os.path.join('LoggedData', 'PredictionForNewData.txt'), 'a+')
+
+        try:
+            y_proba = self.model.predict(input_array)
+            y_pred = np.argmax(y_proba, axis=1)
+
+            Logger().log(f, f"Predicted value for the input array {input_array} is {y_pred}")
+
+            print(f"Predicted value for the input array {input_array} is {y_pred}")
+
+            f.close()
+
+        except Exception as e:
+            Logger().log(f, f"Exception occured while predicting the output for the new picture. Exception: {str(e)}")
+            f.close()
+            f.close()
+
     
